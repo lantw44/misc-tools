e2b9ad1aee11c69322b9ceef003b277a73756d9c
Ting-Wei Lan <lantw44@gmail.com>
2016-02-21 12:34:01 +0800

    package: readall() -> read()
    readall() doesn't work with HTTPResponse.

--- package/check-update-chromium.py
+++ package/check-update-chromium.py
@@ -7,18 +7,18 @@ import urllib.request
 
 def get_upstream_version():
     url = 'https://omahaproxy.appspot.com/all?os=linux&channel=stable'
-    data = urllib.request.urlopen(url).readall()
+    data = urllib.request.urlopen(url).read()
     return data.split(b'\n')[1].split(b',')[2].decode()
 
 def get_package_version_cgit():
     cgit_plain_url = 'http://phantom.tfcis.org/~lantw44/cgit/copr-rpm-spec/plain/chromium/chromium/chromium.spec'
-    spec = urllib.request.urlopen(cgit_plain_url).readall().decode()
+    spec = urllib.request.urlopen(cgit_plain_url).read().decode()
     return spec.split('\nVersion:')[1].split()[0]
 
 def get_package_version_copr():
     copr_api_url = 'https://copr.fedorainfracloud.org/api'
     url = copr_api_url + '/coprs/lantw44/chromium/monitor/'
-    data = json.loads(urllib.request.urlopen(url).readall().decode())
+    data = json.loads(urllib.request.urlopen(url).read().decode())
     for pkg in data['packages']:
         if pkg['pkg_name'] == 'chromium':
             pkg_version = pkg['results']['fedora-23-x86_64']['pkg_version']
--- package/check-update-vim.py
+++ package/check-update-vim.py
@@ -7,13 +7,13 @@ import urllib.request
 
 def get_upstream_version():
     url = 'http://ftp.vim.org/pub/vim/patches/7.4/README'
-    data = urllib.request.urlopen(url).readall()
+    data = urllib.request.urlopen(url).read()
     return data.split(b'\n')[-2].strip().split(b'  ')[1].decode()
 
 def get_package_version_copr():
     copr_api_url = 'https://copr.fedorainfracloud.org/api'
     url = copr_api_url + '/coprs/lantw44/vim-latest/monitor/'
-    data = json.loads(urllib.request.urlopen(url).readall().decode())
+    data = json.loads(urllib.request.urlopen(url).read().decode())
     for pkg in data['packages']:
         if pkg['pkg_name'] == 'vim':
             pkg_version = pkg['results']['fedora-23-x86_64']['pkg_version']
