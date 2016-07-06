b01bbc17658c68405f51f83512c691517dee0e41
Ting-Wei Lan <lantw44@gmail.com>
2016-07-06 16:55:30 +0800

    package: Add a openssl updates checker for gcoin-community

---
+++ package/check-update-openssl.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+import pkg_version
+import sys
+
+verbose = len(sys.argv) - 1
+latest = pkg_version.read_anitya_version('fedora/openssl')
+packaged = pkg_version.read_copr_version(
+    'lantw44/gcoin-community', 'gcoin-compat-openssl', 'epel-7-x86_64')
+exit(pkg_version.write_package_status(latest, packaged, 'openssl', verbose))
