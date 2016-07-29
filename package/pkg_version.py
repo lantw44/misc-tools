5cb3645c46e47493db66598975c5b3a2f3de7fac
Ting-Wei Lan <lantw44@gmail.com>
2016-07-30 01:43:07 +0800

    package: Switch to Copr API v2
    Copr API v1 has been broken for many days without being fixed.

--- package/pkg_version.py
+++ package/pkg_version.py
@@ -54,22 +54,64 @@ def read_anitya_version(project_id,
     data = json.loads(data.decode())
     return data['version']
 
-def read_copr_version(repo, package, chroot, raw = False,
-        instance = 'https://copr.fedorainfracloud.org/api'):
-    url = '{}/coprs/{}/monitor/'.format(instance, repo)
-    data = urllib.request.urlopen(url).read()
+def read_copr_version(repo, package, chroot, raw = False, debug = False,
+        instance = 'https://copr.fedorainfracloud.org/api_2'):
+    if repo.startswith('@'):
+        project_group, project_name = repo.split('@')[1].split('/')
+        projects_query = {'group': project_group, 'name': project_name}
+    elif repo.startswith('g/'):
+        project_group, project_name = repo.split('/')[1:]
+        projects_query = {'group': project_group, 'name': project_name}
+    else:
+        project_owner, project_name = repo.split('/')
+        projects_query = {'owner': project_owner, 'name': project_name}
+    projects_query_encoded = urllib.parse.urlencode(projects_query)
+    projects_url = '{}/projects?{}'.format(instance, projects_query_encoded)
+    data = urllib.request.urlopen(projects_url).read()
     data = json.loads(data.decode())
-    for pkg in data['packages']:
-        if pkg['pkg_name'] == package:
-            pkg_version = pkg['results'][chroot]['pkg_version']
-            if raw:
+    project_id = data['projects'][0]['project']['id']
+    build_offset = 0
+    build_limit = 10
+    while True:
+        if build_offset % build_limit == 0:
+            builds_query = {
+                'project_id': project_id,
+                'offset': build_offset,
+                'limit': build_limit}
+            builds_query_encoded = urllib.parse.urlencode(builds_query)
+            builds_url = '{}/builds?{}'.format(instance, builds_query_encoded)
+            builds_data = urllib.request.urlopen(builds_url).read()
+            builds_data = json.loads(builds_data.decode())
+        if build_offset % build_limit >= len(builds_data['builds']):
+            break
+        build_detail = builds_data['builds'][build_offset % build_limit]
+        build_offset += 1
+        build_id = build_detail['build']['id']
+        build_package_name = build_detail['build']['package_name']
+        build_package_version = build_detail['build']['package_version']
+        if debug:
+            print('{}'.format(build_offset), end = '\t')
+            print('{}'.format(build_package_name), end = '\t')
+            print('{}'.format(build_package_version), end = '\n')
+        if build_package_name != package:
+            continue
+        build_tasks_query = {'build_id': build_id}
+        build_tasks_query_encoded = urllib.parse.urlencode(build_tasks_query)
+        build_tasks_url = '{}/build_tasks?{}'.format(
+            instance, build_tasks_query_encoded)
+        build_tasks_data = urllib.request.urlopen(build_tasks_url).read()
+        build_tasks_data = json.loads(build_tasks_data.decode())
+        for build_task in build_tasks_data['build_tasks']:
+            build_task_chroot_name = build_task['build_task']['chroot_name']
+            if build_task_chroot_name == chroot:
+                pkg_version = build_package_version
+                if not raw:
+                    pkg_version = re.sub('\.centos', '', pkg_version)
+                    pkg_version = re.sub('\.el[0-9]+$', '', pkg_version)
+                    pkg_version = re.sub('\.fc[0-9]+$', '', pkg_version)
+                    pkg_version = re.sub('-.*$', '', pkg_version)
+                    pkg_version = re.sub('^.*:', '', pkg_version)
                 return pkg_version
-            pkg_version = re.sub('\.centos', '', pkg_version)
-            pkg_version = re.sub('\.el[0-9]+$', '', pkg_version)
-            pkg_version = re.sub('\.fc[0-9]+$', '', pkg_version)
-            pkg_version = re.sub('-.*$', '', pkg_version)
-            pkg_version = re.sub('^.*:', '', pkg_version)
-            return pkg_version
 
 def write_package_status(latest, packaged, name, verbose = False):
     out_of_date = LooseVersion(packaged) < LooseVersion(latest)
