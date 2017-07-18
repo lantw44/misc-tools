bf22ee4e7ab47875bc779562d3baea0dae377d96
Ting-Wei Lan <lantw44@gmail.com>
2017-07-18 20:38:51 +0800

    package: Update to Fedora 26

--- package/check-update-chromium.py
+++ package/check-update-chromium.py
@@ -6,5 +6,5 @@ import sys
 verbose = len(sys.argv) - 1
 latest = pkg_version.read_chrome_version('linux', 'stable')
 packaged = pkg_version.read_copr_version(
-    'lantw44/chromium', 'chromium', 'fedora-24-x86_64')
+    'lantw44/chromium', 'chromium', 'fedora-26-x86_64')
 exit(pkg_version.write_package_status(latest, packaged, 'chromium', verbose))
