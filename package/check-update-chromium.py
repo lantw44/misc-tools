49588fd81b49a4945c97069e963ce83a5a6d7010
Ting-Wei Lan <lantw44@gmail.com>
2023-11-26 11:19:52 +0800

    package: Update to Fedora 39

--- package/check-update-chromium.py
+++ package/check-update-chromium.py
@@ -6,5 +6,5 @@ import sys
 verbose = len(sys.argv) - 1
 latest = pkg_version.read_chrome_version('linux', 'stable')
 packaged = pkg_version.read_copr_version(
-    'lantw44/chromium', 'chromium', 'fedora-37-x86_64')
+    'lantw44/chromium', 'chromium', 'fedora-39-x86_64')
 exit(pkg_version.write_package_status(latest, packaged, 'chromium', verbose))
